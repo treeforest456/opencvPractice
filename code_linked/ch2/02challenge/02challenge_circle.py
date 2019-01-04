0c41b19eea220145620c0003aca3a7863db63ac8
tree <treeforest456@gmail.com>
2019-01-03 18:58:47 -0800

    got the drawing board working and can change color and clear the canvas

--- code_linked/ch2/02challenge/02challenge_circle.py
+++ code_linked/ch2/02challenge/02challenge_circle.py
@@ -2,27 +2,26 @@ import cv2
 import numpy as np
 
 canvas = np.ones([500, 500, 3], 'uint8') * 255
-start = None
-moving = None
-end = None
+point = None
+radius = 2
+color = (0, 0, 255)
 pressed_flag = False
 
 def click (event, x, y, flags, param):
-	global canvas, pressed_flag, start, moving, end
+	global canvas, pressed_flag, point, radius, color
 	if event == cv2.EVENT_LBUTTONDOWN:
 		print('pressed')
-		start = (x, y)
-		print(start)
+		point = (x, y)
+		print(point)
 		pressed_flag = True
+		cv2.circle(canvas, point, radius, color, 2)
 	elif event == cv2.EVENT_MOUSEMOVE and pressed_flag == True:
-		moving = (x, y)
-		cv2.rectangle(canvas, start, moving, (0, 0, 255), 3)
+		point = (x, y)
+		cv2.circle(canvas, point, radius, color, 2)
 	elif event == cv2.EVENT_LBUTTONUP:
 		print('up')
-		end = (x, y)
+		point = (x, y)
 		pressed_flag = False
-		canvas = np.ones([500, 500, 3]) * 255
-		cv2.rectangle(canvas, start, end, (0,0,255), 3)
 
 cv2.namedWindow('canvas')
 cv2.setMouseCallback('canvas', click)
@@ -30,7 +29,13 @@ cv2.setMouseCallback('canvas', click)
 while True:
 	cv2.imshow('canvas', canvas)
 	key_press = cv2.waitKey(1)
+	if key_press & 0xFf == ord('r'):
+		color = (0, 0, 255)
 	if key_press & 0xFF == ord('q'):
 		break
+	if key_press & 0xFF == ord('c'):
+		canvas = np.ones([500, 500, 3]) * 255
+	if key_press & 0xFF == ord('b'):
+		color = (255, 0, 0)
 
 cv2.destroyAllWindows()
